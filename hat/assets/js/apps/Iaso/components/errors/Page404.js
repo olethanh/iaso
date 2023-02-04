9fc7edfeb4dee2b112ff3842ce2b86c767cdcff4
Quang Son Le <qle@bluesquarehub.com>
2023-02-04 19:06:17 +0100

    chore: clean up code

--- hat/assets/js/apps/Iaso/components/errors/Page404.js
+++ hat/assets/js/apps/Iaso/components/errors/Page404.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { fetchCurrentUser } from '../../domains/users/actions';
 import SidebarMenu from '../../domains/app/components/SidebarMenuComponent';
 import PageError from './PageError';
-import { useCurrentUser } from '../../utils/usersUtils';
+import { useCurrentUser } from '../../utils/usersUtils.ts';
 
 /* Wrap PageError so we can display the sidebar */
 const Page404 = ({ location }) => {
