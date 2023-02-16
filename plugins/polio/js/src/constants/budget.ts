1eca306da16a4f81edbe796df67f97656ede993d
Quang Son Le <qle@bluesquarehub.com>
2023-02-16 12:06:26 +0100

    POLIO-793: add explanatory comment

--- plugins/polio/js/src/constants/budget.ts
+++ plugins/polio/js/src/constants/budget.ts
@@ -1,3 +1,4 @@
+// TODO cleanup unused states when we move to prod
 export const BUDGET_STATES = [
     'budget_requested',
     'who_sent_budget',
@@ -30,23 +31,16 @@ export const BUDGET_REQUEST = [
 
 export const RRT_REVIEW = [
     'submitted_to_rrt',
-    'feedback_sent_to_gpei',
-    're_submitted_to_rrt',
     'submission_to_orpg_operations_1',
 ];
 
 export const ORPG_REVIEW = [
-    'feedback_sent_to_rrt1',
     'submitted_to_orpg',
     'submission_to_orpg_operations_2',
-    'feedback_sent_to_rrt3',
-    're_submission_to_orpg_operations_2',
 ];
 
 export const REVIEW_FOR_APPROVAL = [
     'submitted_for_approval',
-    'feedback_sent_to_orpg_operations_unicef',
-    'feedback_sent_to_orpg_operations_who',
     'approved_by_who',
     'approved_by_unicef',
     'approved',
