a3121097c79342220ca9586c9bd747e4ecedaa13
Bram Jans <ik@bramjans.com>
2023-10-13 09:52:24 +0200

    TRIA-7 Small fix and Black formatting

--- hat/api_import/migrations/0002_alter_apiimport_user.py
+++ hat/api_import/migrations/0002_alter_apiimport_user.py
@@ -6,16 +6,20 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('api_import', '0001_squashed_0032_auto_20210611_0951'),
+        ("api_import", "0001_squashed_0032_auto_20210611_0951"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='apiimport',
-            name='user',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='apiimports', to=settings.AUTH_USER_MODEL),
+            model_name="apiimport",
+            name="user",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="apiimports",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
--- hat/settings.py
+++ hat/settings.py
@@ -253,7 +253,7 @@ WSGI_APPLICATION = "hat.wsgi.application"
 DB_NAME = os.environ.get("RDS_DB_NAME", "iaso")
 DB_USERNAME = os.environ.get("RDS_USERNAME", "postgres")
 DB_PASSWORD = os.environ.get("RDS_PASSWORD", None)
-DB_HOST = os.environ.get("RDS_HOSTNAME", "localhost")
+DB_HOST = os.environ.get("RDS_HOSTNAME", "db")
 DB_PORT = os.environ.get("RDS_PORT", 5432)
 SNS_NOTIFICATION_TOPIC = os.environ.get("SNS_NOTIFICATION_TOPIC", None)
 
